27aba62daacf64b72bf0add4c3e99b6892f528a1
muna <munaa98@gmail.com>
2022-03-13 20:19:45 +0000

    last commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ const About = () => {
       <div id="about">
         <h1>About</h1>
         <h2>Welcome to the Media Store.</h2>
-        Have a look at what media you are intersted it and just pop it in your basket!
+        My name is Muna and this is my brand new online Media Store. Have a look at what media you are intersted it and just pop it in your basket!
       </div>
     );
   };
